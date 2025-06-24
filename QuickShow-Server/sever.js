056d6341c55d09edf3547715f2f7db4d00b5d1f2
Prajwal Pokhrel <prajwal01532@gmail.com>
2025-06-24 13:23:09 +0545

    server added at 10 gatey

---
+++ QuickShow-Server/sever.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+const app=express();
+const port =3000;
+
+await connectDB()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware())
+
+//APi routes 
+app.get('/' , (req,res)=>res.send("Server is Live!"))
+// Set up the "/api/inngest" (recommended) routes with the serve handler
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.listen(port, ()=>console.log(`Server is listening at http:localhost:${port}`));
